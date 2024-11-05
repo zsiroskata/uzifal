f0fda79bcd9094720628955889642c02d858d1c5
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2024-11-05 09:33:56 +0100

    Update get-messages.php

--- forras/backend/get-messages.php
+++ forras/backend/get-messages.php
@@ -1,7 +1,16 @@
 <?php
+require_once "../connect.php";
 
+$result = mysqli_query($conn, "SELECT name, content, image_path, created_at FROM messages ORDER BY created_at DESC");
+$messages = [];
 
+while ($row = mysqli_fetch_assoc($result)) {
+    $messages[] = $row;
+}
 
+echo json_encode($messages);
 
-// Kapcsolat bezárása
 mysqli_close($conn);
+?>
+
+
